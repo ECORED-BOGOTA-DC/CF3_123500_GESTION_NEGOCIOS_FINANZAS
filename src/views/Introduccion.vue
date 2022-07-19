3230834875a5695301dcf94165469178fbba3a44
giorgio1966 <jehaylock@misena.edu.co>
2022-07-18 23:45:01 -0500

    Primer avance
    Primer avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -3,11 +3,16 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
   
-    .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
-        span
+    .titulo-principal(data-aos="fade-up-right")
+      .titulo-principal__numero.color-acento-contenido
+        span 
           i.fas.fa-info
       h1 Introducción
+    p.mb-5 Se inicia realizando un recorrido por los elementos esenciales a tratar en este componente formativo, explicados a través del siguiente video:
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Leyenda del video
     
 </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,109 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal.color-acento-contenido(data-aos="fade")
       .titulo-principal__numero
-        span 2
-      h1 Titulo de primer nivel
+        span.text-dark 2
+      h1 Planes tácticos
+    p.mb-5 Enfatizan las operaciones actuales de varias partes de la organización. El mediano plazo generalmente se define como un periodo de tiempo que se extiende aproximadamente un año o más en el futuro. Los gerentes utilizan los planes tácticos para delinear lo que deben hacer las diversas partes de la organización para que la organización tenga éxito en algún momento dentro de un año o más. Los planes tácticos generalmente se desarrollan en las áreas de:
+    
+    .row.justify-content-center
+      .col-xxl-5.col-xxl-5col-lg-5.col-md-6.col-sm-8.col-10
+        img.mb-4(src='@/assets/curso/tema1/imagen16.png')  
+      .col-xxl-3.col-xxl-3.col-lg-3.col-md-3.col-sm-4.col-12
+        ul.lista-ul--color
+          li 
+            i.fas.fa-folder-open
+            | Producción
+          li 
+            i.fas.fa-folder-open
+            | Marketing
+          li 
+            i.fas.fa-folder-open
+            | Talento humano
+          li 
+            i.fas.fa-folder-open
+            | Finanzas 
+          li 
+            i.fas.fa-folder-open
+            | Instalaciones de la planta
+    .row.justify-content-center.mb-5
+      .col-xxl-5.col-xxl-5.col-lg-5.col-md-11.col-sm-12.col-12
+        p Los percances que puedan ocurrir durante la ejecución de un plan táctico deben estar cubiertos por incertidumbres moderadas y pueden estar más cerca del control de la dirección. Esos percances, en conjunto con sus posibles consecuencias, se denominan “riesgos tácticos”.
+        p Incertidumbre: va implícitamente desde una falta de certeza absoluta hasta la imprecisión para excluir suposiciones. La duda sugiere incertidumbre en ausencia de certeza absoluta o prueba. Es mucho más difícil tomar decisiones en tiempos de incertidumbre, muchos gerentes de empresas se reprimen de tomar una para evitar crear problemas. La incertidumbre, como se la conoce comúnmente, se trata de no saber eventos futuros.
+      .col-xxl-5.col-xxl-5.col-lg-5.col-md-11.col-sm-12.col-12
+        img(src='@/assets/curso/tema1/imagen18.png')
+    p.my-5 Tomar un riesgo puede resultar en una ganancia o una pérdida porque se conocen los resultados probables, mientras que la incertidumbre viene con probabilidades desconocidas.
+    .row.justify-content-center
+      .col-lg-10
+        .cajon.color-primario.p-4.mb-4.fondo12
+          p El plan táctico describe las tácticas que la organización planea usar para lograr las ambiciones descritas en el plan estratégico. Es un documento de mediano alcance (más de un año), de bajo nivel, que responde a cómo voy hacer, cómo voy a ejecutar, cómo voy a lograr la meta y alcanzar los objetivos que se esbozan en los planes estratégicos. La creación de planes tácticos suele estar a cargo de gerentes de nivel medio.
+          p El plan táctico incluye los planes de acción para alcanzar las metas/objetivos estratégicos que conducen al logro de planes estratégicos. Estos son más generales o más amplios que los planes operativos.
+    .row.justify-content-center
+      .col-xxl-6.col-xl-6.col-lg-6.col-md-8
+        h4.color-texto.text-center Para elaborar un plan táctico, tenga en cuenta los siguientes elementos:
+        img(src='@/assets/curso/tema2/subraya.svg')
+    .tabla-c.my-5
+      table
+        tr
+          th
+            h4 Objetivos específicos con duración.
+          td 
+            p El plan táctico dividirá esta amplia ambición en objetivos más pequeños y factibles. Los objetivos deben ser muy específicos y tener tiempos fijos para estimular la acción.
+        tr
+          th 
+            h4 Presupuesto
+          td 
+            p El plan táctico debe relacionar el presupuesto que requiere para lograr los objetivos trazados en el plan estratégico. Esto debe incluir el presupuesto para:
+            ul.lista-ul
+              li 
+                i.lista-ul__vineta
+                | Contratación de personal.
+              li
+                i.lista-ul__vineta
+                | Publicidad.
+              li 
+                i.lista-ul__vineta
+                | Abastecimiento.
+              li 
+                i.lista-ul__vineta
+                | Producción.
+              li 
+                i.lista-ul__vineta
+                | Papelería.
+              li 
+                i.lista-ul__vineta
+                | Funcionamiento de las operaciones diarias de la empresa. 
+        tr
+          th 
+            h4 <i>Marketing</i>
+          td 
+            p Hacer uso de la estrategia inmediata de <i>marketing</i>, las relaciones públicas, la publicidad y la venta minorista. Su alcance debe estar alineado con los objetivos descritos anteriormente.
+    .row.ampliar.justify-content-center.p-5.fondo11
+      .col-xxl-4.col-xl-4.col-lg-5.col-md-10.col-sm-11.col-12
+        p Como se mencionó al principio, los planes tácticos se desarrollan para apoyar la implementación de los planes estratégicos, los cuales son desarrollados por líderes de las dependencias. Son más específicos y concretos que los planes estratégicos.
+        p El éxito de los planes estratégicos y tácticos depende del logro de los objetivos operativos.
+        .bloque-texto-b.color-secundario.p-4
+          .bloque-texto-b__texto
+            i.fas.fa-quote-left
+            p.mb-0 Según el pensamiento de Torres (2015, p. 211), el plan táctico es el medio que utilizan los colaboradores de la organización para traducir los planes estratégicos en objetivos específicos para determinadas unidades administrativas.
+            i.fas.fa-quote-right
+      .col-xxl-1.col-xl-1.col-lg-1.col-md-12.col-sm-12.col-12
+      .col-xxl-3.col-xl-3.col-lg-4.col-md-6.col-sm-7.col-8
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 3. 
+        img(src='@/assets/curso/tema2/figura1.svg')
+    p.my-5.text-center Quien lleve a cabo la formulación de un plan táctico tendrá en cuenta los siguientes elementos de la organización:
+    .row.justify-content-center
+      .col-xxl-4.col-xl-4.col-lg-4.col-md-10.col-12.me-3.my-3
+        img(src='@/assets/curso/tema2/imagenes1.svg')
+      .col-xxl-4.col-xl-4.col-lg-4.col-md-10.col-12.me-3.my-3
+        img(src='@/assets/curso/tema2/imagenes2.svg')
+      .col-xxl-1.col-xl-1.col-lg-1.col-md-3.col-3.my-3
+        img(src='@/assets/curso/tema2/imagen3.svg')
+    p.my-4 Por lo general, se crea un plan táctico cuando la organización quiere abordar una meta a corto plazo que desea alcanzar en un año o menos. Sin embargo, también debería ayudar a la empresa a lograr sus objetivos a mediano o largo plazo.  
 
 </template>
 
